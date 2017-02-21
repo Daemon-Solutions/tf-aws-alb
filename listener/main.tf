d21bcc194f97d6a71e14bf6868950c54bccb8c2e
Pawel Fiuto <pawel@bashton.com>
2017-02-21 09:16:13 +0000

    Wrong var name

--- listener/main.tf
+++ listener/main.tf
@@ -1,9 +1,10 @@
 resource "aws_alb_listener" "alb_listener" {
+  count             = "${var.is_enabled}"
   load_balancer_arn = "${var.load_balancer_arn}"
   port              = "${var.listener_port}"
   protocol          = "${var.listener_protocol}"
-  ssl_policy        = "${var.ssl_policy}"
-  certificate_arn   = "${var.certificate_arn}"
+  ssl_policy        = "${var.listener_certificate_arn != "" ? var.listener_ssl_policy : ""}"
+  certificate_arn   = "${var.listener_certificate_arn}"
 
   default_action {
     target_group_arn = "${var.target_group_arn}"
