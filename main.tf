3c3cdf8c12bec94d5f8bc864e26b2116540fc49d
Pawel Fiuto <pawel@bashton.com>
2017-02-23 17:36:38 +0000

    Subnets should be a list

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_alb" "alb" {
   name                       = "${var.name}-alb"
   internal                   = "${var.internal}"
   security_groups            = ["${var.security_groups}"]
-  subnets                    = "${var.subnets}"
+  subnets                    = ["${var.subnets}"]
   idle_timeout               = "${var.idle_timeout}"
   enable_deletion_protection = "${var.enable_deletion_protection}"
 
